d4b9280ea6b1b2c9ddc7065a8c285a1626c32c5a
Kyle Maxwell <krmaxwell@gmail.com>
2014-04-27 05:09:31 +0000

    Starting to fill out object code

--- ex43/ex43.py
+++ ex43/ex43.py
@@ -1,16 +1,30 @@
+from sys import exit
+from random import randint
+
+
 class Scene(object):
 
     def enter(self):
-        pass
+        print "This scene is not yet configured. Subclass it and implement enter()."
+        exit(1)
 
 
 class Engine(object):
 
     def __init__(self, scene_map):
-        pass
+        print "Engine __init__ has scene_map", scene_map
+        self.scene_map = scene_map
 
     def play(self):
-        pass
+        current_scene = self.scene_map.opening_scene()
+        print "Play's first scene", current_scene
+
+        while True:
+            print "\n-----"
+            next_scene_name = current_scene.enter()
+            print "next scene", next_scene_name
+            current_scene = self.scene_map.next_scene(next_scene_name)
+            print "map returns new scene", current_scene
 
 
 class Death(Scene):
